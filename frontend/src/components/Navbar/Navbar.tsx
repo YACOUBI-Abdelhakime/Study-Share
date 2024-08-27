3c062444518a0d63ca5215694121d160babaca95
YACOUBI-Abdelhakime <hakim.199911@gmail.com>
2024-08-27 23:02:19 +0200

    Collapse navbar when clicking on link

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -8,29 +8,43 @@ import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { useRef } from "react";
 
 export default function Navbar() {
   const { t } = useTranslation();
   const user = useSelector((state: any) => state.userReducer.user);
+  const navbarRef = useRef<HTMLDivElement>(null);
 
   const onLogoutClicked = () => {
     localStorage.clear();
     window.location.reload();
   };
+
+  const closeNavbar = () => {
+    navbarRef.current?.classList.remove("show");
+  };
   return (
     <nav
-      className="navbar navbar-expand-md navbar-light gradient-light-vertical p-0 fixed-top"
+      className="navbar navbar-expand-md navbar-light  bg-light p-0 fixed-top"
       style={{ height: "55px" }}
     >
       <div className="container-fluid px-5 pb-0">
         <div>
-          <Link to="/">
+          {user ? (
+            <Link to="/">
+              <img
+                src="assets/logo-1x2.svg"
+                alt="Study Share Logo"
+                className="navbar-logo"
+              />
+            </Link>
+          ) : (
             <img
               src="assets/logo-1x2.svg"
               alt="Study Share Logo"
               className="navbar-logo"
             />
-          </Link>
+          )}
         </div>
         <button
           className="navbar-toggler"
@@ -43,29 +57,43 @@ export default function Navbar() {
         >
           <FontAwesomeIcon icon={faBars} />
         </button>
-        <div className="collapse navbar-collapse" id="navbarContent">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <Link className="nav-link active" to="/">
-                {t("home")}
-              </Link>
-            </li>
-            <li className="nav-item d-block d-lg-none">
-              <Link className="nav-link" to="/messages">
-                {t("messages")}
-              </Link>
-            </li>
-            <li className="nav-item d-block d-lg-none">
-              <Link className="nav-link" to="/notifications">
-                {t("notifications")}
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/add-publication">
-                {t("addPublication")}
-              </Link>
-            </li>
-          </ul>
+        <div
+          className="collapse navbar-collapse bg-light"
+          id="navbarContent"
+          ref={navbarRef}
+        >
+          {user && (
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <Link className="nav-link active" to="/" onClick={closeNavbar}>
+                  {t("home")}
+                </Link>
+              </li>
+              <li className="nav-item d-block d-lg-none">
+                <Link className="nav-link" to="/messages" onClick={closeNavbar}>
+                  {t("messages")}
+                </Link>
+              </li>
+              <li className="nav-item d-block d-lg-none">
+                <Link
+                  className="nav-link"
+                  to="/notifications"
+                  onClick={closeNavbar}
+                >
+                  {t("notifications")}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/add-publication"
+                  onClick={closeNavbar}
+                >
+                  {t("addPublication")}
+                </Link>
+              </li>
+            </ul>
+          )}
           <div className="col">
             {user ? (
               <div className="float-end pb-md-0 pb-2">
@@ -78,16 +106,28 @@ export default function Navbar() {
                 >
                   <FontAwesomeIcon icon={faRightFromBracket} rotation={180} />
                 </button>
-                <Link className="btn btn-primary" to="/profile">
+                <Link
+                  className="btn btn-primary"
+                  to="/profile"
+                  onClick={closeNavbar}
+                >
                   <FontAwesomeIcon icon={faCircleUser} size="xl" />
                 </Link>
               </div>
             ) : (
               <div className="float-end pb-md-0 pb-2">
-                <Link className="btn btn-outline-primary mx-2" to="/login">
+                <Link
+                  className="btn btn-outline-primary mx-2"
+                  to="/login"
+                  onClick={closeNavbar}
+                >
                   {t("login")}
                 </Link>
-                <Link className="btn btn-primary" to="/register">
+                <Link
+                  className="btn btn-primary"
+                  to="/register"
+                  onClick={closeNavbar}
+                >
                   {t("signup")}
                 </Link>
               </div>
